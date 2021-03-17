ab6fdaa17d22a856d18d38ff0c4e043984e7524e
Chandu <rrtutor.me@gmail.com>
2021-03-17 10:36:45 +0530

    Updated Main Activity Imports

--- app/src/main/java/com/rrtutors/cicd2/MainActivity.kt
+++ app/src/main/java/com/rrtutors/cicd2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rrtutors.cicd2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.rrtutors.cicd2.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
